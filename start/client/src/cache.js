ccd6efb4f919907fd4b174586c461b647bfeafd8
Lilia Lomaka <llomaka80@gmail.com>
2022-09-09 13:30:24 +0300

    Completing client fullstack tutorial

--- start/client/src/cache.js
+++ start/client/src/cache.js
@@ -1,3 +1,44 @@
-import { InMemoryCache } from '@apollo/client';
+import { InMemoryCache, makeVar } from '@apollo/client';
 
-export const cache = new InMemoryCache({});
+export const cache = new InMemoryCache({
+    typePolicies: {
+        Query: {
+            fields: {
+                isLoggedIn: {
+                    read() {
+                        return isLoggedInVar();
+                    },
+                },
+
+                cartItems: {
+                    read() {
+                        return cartItemsVar();
+                    },
+                },
+                launches: {
+                    keyArgs: false,
+                    merge(existing, incoming) {
+                        let launches = [];
+                        if (existing && existing.launches) {
+                            launches = launches.concat(existing.launches);
+                        }
+                        if (incoming && incoming.launches) {
+                            launches = launches.concat(incoming.launches);
+                        }
+                        return {
+                            ...incoming,
+                            launches,
+                        };
+                    },
+                },
+            },
+        },
+    },
+});
+
+// Initializes to true if localStorage includes a 'token' key,
+// false otherwise
+export const isLoggedInVar = makeVar(!!localStorage.getItem('token'));
+
+// Initializes to an empty array
+export const cartItemsVar = makeVar([]);
